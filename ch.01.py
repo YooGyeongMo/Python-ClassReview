27d680e2203e280a87b6b2cdcd6c523eb95e9593
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-04-09 15:30:32 +0900

    ch.01

---
+++ ch.01.py
@@ -0,0 +1,17 @@
+print("Hello Python")
+
+print("3 x 1 = 3")
+print("3 x 2 = 6")
+print("3 X 3 = 9")
+
+# 출력 방식 이해하기
+print("2 + 5 =7")
+print("2 + 5 =", 2+5)
+
+print("9 X 1 =", 9*1)
+print("9 X 2 =", 9*2)
+print("9 X 3 =", 9*3)
+print("9 X 4 =", 9*4)
+print("9 X 5 =", 9*5)
+print("9 X 6 =", 9*6)
+print("9 X 7 =", 9*7)
